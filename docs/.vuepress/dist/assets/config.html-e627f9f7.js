ea6388ebbaafdab5f3f306615ddfde4f1eea3f3c
PZQ <875035774@qq.com>
2023-09-26 12:03:09 +0800

    提交

---
+++ docs/.vuepress/dist/assets/config.html-e627f9f7.js
@@ -0,0 +1,41 @@
+import{_ as n,o as s,c as a,f as t}from"./app-d2c8b486.js";const e={},p=t(`<div class="hint-container tip"><p class="hint-container-title">全局配置</p><p>使用前请设置全局配置、将页面放置在<code>els-container</code>中<br> 包含默认查询、验证、导出功能、缩减大部分重复的代码</p></div><div class="language-typescript line-numbers-mode" data-ext="ts"><pre class="language-typescript"><code>  app<span class="token punctuation">.</span>config<span class="token punctuation">.</span>globalProperties<span class="token punctuation">.</span>$lessConfig <span class="token operator">=</span> <span class="token punctuation">{</span>
+      api<span class="token operator">:</span><span class="token punctuation">{</span> <span class="token comment">//api接口返回结构</span>
+        code<span class="token operator">:</span><span class="token string">&#39;ResultCode&#39;</span><span class="token punctuation">,</span><span class="token comment">//接口返回状态码</span>
+        message<span class="token operator">:</span><span class="token string">&#39;ResultMessage&#39;</span><span class="token punctuation">,</span><span class="token comment">//接口返回说明</span>
+        data<span class="token operator">:</span><span class="token string">&#39;Data&#39;</span><span class="token punctuation">,</span><span class="token comment">//接口返回数据</span>
+        successCode<span class="token operator">:</span><span class="token string">&#39;0&#39;</span><span class="token punctuation">,</span><span class="token comment">//状态码成功值</span>
+        eventData<span class="token operator">:</span><span class="token punctuation">{</span><span class="token comment">//接口返回事件</span>
+          data<span class="token operator">:</span><span class="token string">&#39;EventActionData&#39;</span><span class="token punctuation">,</span><span class="token comment">//数据字段</span>
+          data_key<span class="token operator">:</span><span class="token string">&#39;Name&#39;</span><span class="token punctuation">,</span><span class="token comment">//事件名：[&#39;Alert&#39;,&#39;TargetUrl&#39;,&#39;RefreshTable&#39;,&#39;CloseDialog&#39;,&#39;CloseDrawer&#39;]</span>
+          data_value<span class="token operator">:</span><span class="token string">&#39;value&#39;</span><span class="token comment">//事件值</span>
+        <span class="token punctuation">}</span>
+      <span class="token punctuation">}</span><span class="token punctuation">,</span>
+      upload<span class="token operator">:</span><span class="token punctuation">{</span> <span class="token comment">//上传组件</span>
+        url<span class="token operator">:</span><span class="token string">&#39;http://manage.ybt2023.com/Home/TestUpload&#39;</span><span class="token punctuation">,</span><span class="token comment">//默认上传地址</span>
+        data<span class="token operator">:</span><span class="token string">&#39;UploadInfo&#39;</span><span class="token punctuation">,</span><span class="token comment">//上传完成数据返回在api接口中的data中</span>
+        data_path<span class="token operator">:</span><span class="token string">&#39;ResourcePath&#39;</span><span class="token punctuation">,</span><span class="token comment">//上传完返回地址</span>
+        data_md5<span class="token operator">:</span><span class="token string">&#39;Md5Value&#39;</span><span class="token punctuation">,</span><span class="token comment">//上传完返回md5</span>
+      <span class="token punctuation">}</span><span class="token punctuation">,</span>
+      menu<span class="token operator">:</span><span class="token punctuation">{</span> <span class="token comment">//菜单：导航菜单、工具栏菜单、右键菜单</span>
+        id<span class="token operator">:</span><span class="token string">&#39;MenuID&#39;</span><span class="token punctuation">,</span><span class="token comment">//菜单ID</span>
+        name<span class="token operator">:</span><span class="token string">&#39;MenuName&#39;</span><span class="token punctuation">,</span><span class="token comment">//菜单名称</span>
+        action<span class="token operator">:</span><span class="token string">&#39;ActionName&#39;</span><span class="token punctuation">,</span><span class="token comment">//菜单动作名称</span>
+        actionType<span class="token operator">:</span><span class="token string">&#39;ActionType&#39;</span><span class="token punctuation">,</span><span class="token comment">//菜单动作类型：保存、新增、修改、删除</span>
+        icon<span class="token operator">:</span><span class="token string">&#39;ImageUrl&#39;</span><span class="token punctuation">,</span><span class="token comment">//菜单图标</span>
+        buttonColor<span class="token operator">:</span><span class="token string">&#39;ButtonColor&#39;</span><span class="token punctuation">,</span><span class="token comment">//按钮颜色</span>
+        buttonType<span class="token operator">:</span><span class="token string">&#39;ButtonType&#39;</span><span class="token punctuation">,</span><span class="token comment">//按钮类型：跳转、保存、查询、导入导出</span>
+        group<span class="token operator">:</span><span class="token string">&#39;Fold&#39;</span><span class="token comment">//组名</span>
+      <span class="token punctuation">}</span><span class="token punctuation">,</span>
+      table<span class="token operator">:</span><span class="token punctuation">{</span> <span class="token comment">//表格</span>
+          menu<span class="token operator">:</span><span class="token string">&#39;PowerMenu&#39;</span><span class="token punctuation">,</span> <span class="token comment">//右键菜单字段</span>
+          avgDay<span class="token operator">:</span><span class="token string">&#39;DayCount&#39;</span><span class="token punctuation">,</span><span class="token comment">//所选时间段天数，用来做平均使用，一般不配置</span>
+          page<span class="token operator">:</span><span class="token punctuation">{</span> <span class="token comment">//分页</span>
+            data<span class="token operator">:</span><span class="token string">&quot;Data&quot;</span><span class="token punctuation">,</span><span class="token comment">//数据</span>
+            pageSize<span class="token operator">:</span><span class="token string">&#39;PageSize&#39;</span><span class="token punctuation">,</span><span class="token comment">//页大小</span>
+            currentPage<span class="token operator">:</span><span class="token string">&#39;PageIndex&#39;</span><span class="token punctuation">,</span><span class="token comment">//当前页</span>
+            total<span class="token operator">:</span><span class="token string">&#39;RecordCountInt&#39;</span><span class="token punctuation">,</span><span class="token comment">//总数</span>
+            pageCount<span class="token operator">:</span><span class="token string">&#39;PageCount&#39;</span><span class="token comment">//页数</span>
+          <span class="token punctuation">}</span>
+      <span class="token punctuation">}</span>
+  <span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),o=[p];function c(l,i){return s(),a("div",null,o)}const u=n(e,[["render",c],["__file","config.html.vue"]]);export{u as default};
---
+++ docs/.vuepress/dist/assets/start.html-b01d922f.js
@@ -0,0 +1,15 @@
+import{_ as n,o as s,c as e,f as a}from"./app-d2c8b486.js";const i={},l=a(`<h3 id="使用包管理器" tabindex="-1"><a class="header-anchor" href="#使用包管理器" aria-hidden="true">#</a> 使用包管理器</h3><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token function">npm</span> i element-plus 
+<span class="token function">npm</span> i element-less
+<span class="token function">npm</span> i lodash
+<span class="token function">npm</span> i xlsx
+<span class="token function">npm</span> i file-saver
+<span class="token function">npm</span> i sortablejs 
+<span class="token function">npm</span> i vuedraggable@next 
+<span class="token function">npm</span> i --save-dev @types/sortablejs
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="选装" tabindex="-1"><a class="header-anchor" href="#选装" aria-hidden="true">#</a> 选装</h3><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token function">npm</span> i vue-fontawesome   
+<span class="token function">npm</span> i fortawesome    
+<span class="token function">npm</span> i @fortawesome/fontawesome-svg-core      
+<span class="token function">npm</span> i @fortawesome/free-solid-svg-icons      
+<span class="token function">npm</span> i @fortawesome/vue-fontawesome@prerelease
+<span class="token function">npm</span> i @fortawesome/free-regular-svg-icons
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,4),c=[l];function t(o,d){return s(),e("div",null,c)}const p=n(i,[["render",t],["__file","start.html.vue"]]);export{p as default};
