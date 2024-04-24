0d7f1f99e5d09498f8bdf95e6af1eb0847c27209
PZQ <875035774@qq.com>
2024-04-24 14:39:33 +0800

    dropdown手机端适配

---
+++ packages/elementui/dropdown/DropdownGroup.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { useAttrs } from 'vue'
+import { useValue } from '../../utlis/use';
+defineOptions({
+    name: 'ElsDropdownGroup',
+    inheritAttrs:false
+})
+
+interface Props {
+}
+
+const props = withDefaults(defineProps<Props>(), {
+
+})
+const { getValue } = useValue()
+const { setValue } = useValue(props)
+const isMobile = getValue<boolean>('isMobile', false)
+const attrs=useAttrs();
+setValue({
+    'type': 'dropdownGroup'
+})
+
+</script>
+<template>
+    <div class="els-dropdown-group" v-if="!isMobile">
+        <slot></slot>
+    </div>
+    <van-dropdown-menu v-else v-bind="attrs">
+        <slot></slot>
+    </van-dropdown-menu>
+</template>
+<style lang="less" scoped>
+.els-dropdown-group{display: flex;gap: 10px;}
+</style>
