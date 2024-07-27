4bb7f886c9ddb0e6b8cc821946603c8008b49be7
PZQ <875035774@qq.com>
2024-07-27 17:10:52 +0800

    修改配置

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -12,7 +12,7 @@ import tag from './plugins/tag'
 const slidebar =  getSidebar(__dirname, '');
 
 export default defineConfig({
-    // base: "/element-less/",
+    base: "/element-less/",
     themeConfig:{
         search:{provider: 'local'},
         siteTitle:'Element-Less',
